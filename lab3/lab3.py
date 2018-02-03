ad4fbeb0c84c27de26901eeb59bb038943928fc6
Robert-PC <eastinr@oregonstate.edu>
2018-02-02 22:02:56 -0800

    finished lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+from sin_plot import plot_sin
+from histo_plot import plot_histo
+from spring_mass_plot import plot_spring_mass
+from sort_sum_plot import plot_sort_sum
+
+
+def show_plots():
+    plot_sin()
+    plot_histo()
+    plot_spring_mass()
+    plot_sort_sum()
 
 
 if __name__ == '__main__':
+    show_plots()
--- lab3/sort_sum_plot.py
+++ lab3/sort_sum_plot.py
@@ -3,16 +3,31 @@
 import timeit
 import matplotlib.pyplot as plt
 import random
-import numpy as np
 
 
 def plot_sort_sum():
     size_array = [1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
     list_arr = []
-    for i in size_array:
-        list_arr.append(random.sample(xrange(i), i))
-        t = timeit.timeit(sorted(list_arr[-1]))
-        print t
+    sort_time_list = []
+    sum_time_list = []
+
+    for i in range(len(size_array)):
+        pop_list = range(1, int(size_array[i]) + 1)
+        rand_list = random.sample(pop_list, len(pop_list))
+        list_arr.append(rand_list)
+        sort_time_list.append(timeit.Timer(lambda: sorted(list_arr[i])).timeit(number=1))
+        sum_time_list.append(timeit.Timer(lambda: sum(list_arr[i])).timeit(number=1))
+
+    plt.semilogx(size_array, sort_time_list, label='sorted function')
+    plt.semilogx(size_array, sum_time_list, label='sum function')
+    plt.grid(True)
+    plt.title("Function Timing")
+    plt.xlabel("List Size")
+    plt.ylabel("Execution Time")
+    plt.legend()
+
+    plt.show()
+
 
 if __name__ == '__main__':
     plot_sort_sum()
