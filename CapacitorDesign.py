7eb55b10e2fcba4b1fb1d1bb0b240bf5078c3bbf
robert <eastinr@oregonstate.edu>
2018-03-07 13:15:13 -0800

    cleaned up serial read

---
+++ CapacitorDesign.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import itertools
+
+
+class CapacitorSet:
+    stringlist = []
+
+    def __init__(self, caplist, parent=None, cap1=None, cap2=None):
+        self.parent = parent
+        self.caplist = caplist
+        self.depth = len(caplist)
+        self.child1 = cap1
+        self.child2 = cap2
+        self.ans = None
+
+        if self.parent == None:
+            try:
+                self.str = str(self.caplist[0])
+            except:
+                self.str = "C"
+            self.func = self.caplist[0]
+            self.name = "root"
+        else:
+            self.str = self.parent.str
+
+    def series(self):
+        self.name = "Series".format(self.depth)
+        self.func = self.calcSeries([self.parent.func, self.caplist[0]])
+        if self.parent.name == self.name:
+            newstr = self.parent.str
+            if newstr[0] == '(' and newstr[-1] == ')':
+                newstr = newstr[1:-1]
+            self.str = "{0} + {1}".format(newstr, self.caplist[0])
+
+        else:
+            self.str = "({0} + {1})".format(self, self.caplist[0])
+        self.stringlist.append((self.str, self.func))
+
+    def parallel(self):
+        self.name = "Parallel".format(self.depth)
+        self.func = self.calcParallel([self.parent.func, self.caplist[0]])
+        if self.parent.name == self.name:
+            newstr = self.parent.str
+            if newstr[0] == '(' and newstr[-1] == ')':
+                newstr = newstr[1:-1]
+            self.str = "{0} || {1}".format(newstr, self.caplist[0])
+
+        else:
+            self.str = "({0} || {1})".format(self, self.caplist[0])
+        self.stringlist.append((self.str, self.func))
+
+    def __str__(self):
+        return self.str
+
+    def __repr__(self):
+        return self.str
+
+    def getcombo(self):
+        if self.depth == 1:
+            return
+        else:
+            c1 = CapacitorSet(self.caplist[1:], self)
+            c2 = CapacitorSet(self.caplist[1:], self)
+            c1.series()
+            c2.parallel()
+            self.setchild(c1, c2)
+            self.child1.getcombo()
+            self.child2.getcombo()
+
+    def setchild(self, child1, child2):
+        self.child1 = child1
+        self.child2 = child2
+
+
+    def calcParallel(self, args):
+        if len(args) == 2:
+            return 1 / (1 / float(args[0]) + 1 / float(args[1]))
+        else:
+            ans = 1 / (1 / float(self.func) + 1 / float(args[0]))
+            return ans
+
+    def calcSeries(self, args):
+        if len(args) == 2:
+            return args[0] + args[1]
+        else:
+            ans = self.func() + args[1]
+            return ans
+
+def findCapacitor(target, max_number, caplist):
+    newlist = itertools.product(caplist, repeat=max_number)
+    dataset = set([])
+    for lst in newlist:
+        cap = CapacitorSet(lst)
+        cap.getcombo()
+        dataset |= set(cap.stringlist)
+    datalist = list(dataset)
+    datalist.sort(key=lambda data: abs(target - data[1]))
+    for datum in datalist:
+        print "{0:.3f} = {1}".format(datum[1], datum[0])
+
+
+if __name__ == '__main__':
+    cap_list = [1, 2, 3, 4]
+    target = 0.45
+    findCapacitor(target, 4, cap_list)
+
+    #for index, func in enumerate(cap.funclist):
+    #    print "{0}\t=\t{1}".format(cap.stringlist[index], func)
--- ME451/readArduino.py
+++ ME451/readArduino.py
@@ -2,32 +2,62 @@
 
 import serial
 import time
+import threading
+import Queue
+import multiprocessing
 
-def readArduino(filename, baud, timeout):
 
-    locations = ['/dev/ttyACM0','/dev/ttyACM1']
-    connected = False
+class SerialThread(threading.Thread):
+    def __init__(self, queue, port, baud):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.port = port
+        self.baud = baud
+
+    def run(self):
+        s = serial.Serial(self.port, self.baud, timeout=0, writeTimeout=0)
+        while True:
+            if s.inWaiting():
+                try:
+                    self.queue.put(s.readline(s.inWaiting()))
+                except ValueError:
+                    pass
+
+def writeFile(dest, queue, timeout):
+    start = time.time()
+    with open(dest, 'w') as destfile:
+        while time.time() - start < timeout:
+            try:
+                destfile.write(queue.get())
+            except:
+                pass
+
+
+def readArduino(queue, filename, baud, timeout):
+    locations = ['/dev/ttyACM0', '/dev/ttyACM1']
 
     for device in locations:
         try:
-            print "Trying...", device
-            ser = serial.Serial(device, baud)
+            tempser = serial.Serial(device, baud)
+            print "Successfully connected to", device
+            portname = device
+            tempser.close()
             break
         except:
             print "Failed to connect on", device
 
-    while not connected:
-        serin = ser.read()
-        connected = True
+    serialdata = SerialThread(queue, portname, baud)
+    serialdata.start()
 
-    text_file = open(filename, 'w')
+    filethread = threading.Thread(target=writeFile, args=(filename, queue, timeout))
+    filethread.start()
 
-    start = time.time()
-    while time.time() - start < timeout:
-        line = ser.readline().decode('utf-8')
-        text_file.write(line)
-        text_file.flush()
+    serialdata.paused = True
 
-    # close the serial connection and text file
-    text_file.close()
-    ser.close()
+
+if __name__ == '__main__':
+    queue = Queue.Queue()
+    filename = "piddelay5000.csv"
+    baud = 9600
+    timeout = 10
+    readArduino(queue, filename, baud, timeout)
